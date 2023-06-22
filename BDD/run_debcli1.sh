141c8d9bcb8ad017026264bde117d1dc9b2d34da
Martin <94961645+SahmMartin@users.noreply.github.com>
2023-06-22 14:50:39 +0200

    Delete BDD directory

--- BDD/run_debcli1.sh
+++
@@ -1,2 +0,0 @@
-docker run --rm -it --network=host -h=127.0.0.1 deb-cli-1 bash
-RUN mysql -u root -p'foo' -h 127.0.0.1
--- BDD/run_mysql.sh
+++
@@ -1,1 +0,0 @@
-docker run --rm -d -p 3306:3306 --env MYSQL_ROOT_PASSWORD=foo -v vol-sql:/var/lib/mysql mysql
